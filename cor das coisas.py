90f055334d5e7025c2a37e328d7ed23f1510a283
Renanito69 <renanbascristian@gmail.com>
2025-04-24 11:49:37 -0300

    bla

---
+++ cor das coisas.py
@@ -0,0 +1,18 @@
+comando = print('\033[;;m')
+# escrita
+
+normal = 0
+negrito = 1
+sublinado = 4
+inverso = 7
+
+# Text / Fundo
+branco = 30 / 40
+vermelho = 31 / 41
+verde = 32 / 42
+amarelo = 33 / 43
+azul = 34 / 44
+roxo = 35 / 45
+ciano = 36 / 46
+cinza = 37 / 47
+
